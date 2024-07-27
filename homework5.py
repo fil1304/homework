50d559330d3e3ce7bb75db4d50057b9b494ea65a
fil1304 <vika.wika.kalinina@mail.ru>
2024-07-27 16:56:10 +0300

    homework5.py

---
+++ homework5.py
@@ -0,0 +1,7 @@
+immutable_var = 1, 2, 3, 4
+print(immutable_var).ю
+#кортеж-является неизменяемым объектом
+mutable_list = ([1,2],3)
+print(mutable_list)23ц
+mutable_list [0][0] = 2
+print(mutable_list)
