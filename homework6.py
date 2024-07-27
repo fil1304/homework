07021be732c1d4596db807734a5d2a0785f4a527
fil1304 <vika.wika.kalinina@mail.ru>
2024-07-27 16:54:34 +0300

    homework6.py

---
+++ homework6.py
@@ -0,0 +1,16 @@
+my_dict = {"Vika":2003, 'Olga': 2004, 'Alexander':2003}
+print(my_dict)
+print(my_dict['Vika'])
+my_dict['Denis']=2000
+my_dict['Natali']=2005
+my_dict ['Alex']=2000
+print(my_dict)
+del my_dict['Vika']
+print(my_dict)
+my_set = {1, 1, 2, 3, 2, 3, True, 'World'}
+print(my_set)
+list=[7, 8, 9, 7]
+list=set(list)
+print(list)
+print(list.discard(7))
+print(list)
