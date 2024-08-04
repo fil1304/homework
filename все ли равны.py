b302ac470fe7807e5269d2de9e6ef557400d289e
fil1304 <vika.wika.kalinina@mail.ru>
2024-08-04 18:43:42 +0300

    доп.дз

--- все ли равны.py
+++ все ли равны.py
@@ -4,10 +4,10 @@ second = int(input('Введите второе число: '))
 print(second)
 third = int(input('Введите третье число: '))
 print(third)
-if first == second and first == third or second == third:
-    print(2)
 if first == second == third:
     print(3)
+elif first == second or first == third or second == third:
+    print(2)
 elif not first == second == third:
     print(0)
 
