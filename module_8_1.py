e9ecc779ace73762cb793829f8fb005932fa10d5
fil1304 <vika.wika.kalinina@mail.ru>
2024-08-28 15:15:13 +0300

    доп.дз

---
+++ module_8_1.py
@@ -0,0 +1,9 @@
+def add_everything_up(a, b):
+    try:
+        print(a + b)
+
+    except TypeError:
+        print(str(a) + str(b))
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
